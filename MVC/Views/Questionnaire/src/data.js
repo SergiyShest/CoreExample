536831172e7b4b3b3d5f16e0dee4daa392ab4790
Shestakov <shest_akow@rambler.ru>
2023-08-29 04:18:52 +0300

    Working

---
+++ MVC/Views/Questionnaire/src/data.js
@@ -0,0 +1,301 @@
+export const  data =  {
+    "Item": {
+        "Id": 1,
+        "Name": "Answer a few questions and see if you or someone you are looking after may be eligible",
+        "Code": "QUESTIONNAIRE",
+
+        "Questions": [
+            {
+                "Order":1,
+                "Name": 'Do you have a current diagnosis of Atopic Dermatitis (Atopic Eczema)?',
+                "Code": "QUESTION",
+                "Description": "Atopic Dermatitis, also known as Atopic Eczema, is a type of inflammation of the skin, resulting in itchy, red, swollen, and cracked skin.",
+                "Schema": {
+                    "type": "object",
+                    "properties": {
+                        "ListProp": {
+                            "type": "number",
+                            "title": "Choose the right Ansver",
+                            "x-display": "radio",
+                            "x-fromData": "context.Items",
+                            "x-itemKey": "Id",
+                            "x-itemTitle": "Name",
+                            "description": "This description is used as a help message."
+                        }
+                    }
+                },
+                "Options": {
+                    "context": {
+                        "Items": [
+                            {
+                                "Id": 1,
+                                "Name": "Yes"
+                            },
+                            {
+                                "Id": 2,
+                                "Name": "No, but currently undergoing medical examinations"
+                            },
+                            {
+                                "Id": 3,
+                                "Name": "No"
+                            }
+                        ]
+                    },
+                    "selectAll": true
+                },
+                "QuestionnaireId": 1,
+                "Id": 1
+            },
+            {
+                "Name": "How old are yow",
+                "Code": "QUESTION",
+                "Description": "",
+                "Order":2,
+                "Schema": {
+                        type: "object",
+                        requred: ["NumProp"],
+                        properties: {
+                          NumProp: {
+                            type: "string",
+                            format: "number",
+                            title: "Input numper please",
+                            description: "Help",
+                          },
+                        },
+
+                },
+                "Options": { },
+                "QuestionnaireId": 1,
+                "Id": 2,
+
+            }
+            ,
+            {   "Id": 3,
+                "Order":3,
+                "Name": 'When where yoy diagnosed Atopic Dermatitis (Atopic Eczema)?',
+                "Code": "QUESTION",
+                "Description": "",
+                "Schema": {
+                    "type": "object",
+                    "properties": {
+                        "ListProp": {
+                            "type": "number",
+                            "title": "Choose the right Ansver",
+                            "x-display": "radio",
+                            "x-fromData": "context.Items",
+                            "x-itemKey": "Id",
+                            "x-itemTitle": "Name",
+                            "description": ""
+                        }
+                    }
+                },
+                "Options": {
+                    "context": {
+                        "Items": [
+                            {
+                                "Id": 1,
+                                "Name": "Less then 6 Months ago"
+                            },
+                            {
+                                "Id": 2,
+                                "Name": "Over 6 Months ago"
+                            }
+                        ]
+                    },
+                    "selectAll": true
+                },
+                "QuestionnaireId": 1
+            },
+            {"Id": 3,
+            "Order":3,
+            "Name": "",
+            "Code": "QUESTION",
+            "Description": "How would you describe your current Atopic Dermatitis (Atopic Eczema)?",
+            "Schema": {
+                "type": "object",
+                "properties": {
+                    "ListProp": {
+                        "type": "number",
+                        "title": "Choose the right Ansver",
+                        "x-display": "radio",
+                        "x-fromData": "context.Items",
+                        "x-itemKey": "Id",
+                        "x-itemTitle": "Name",
+                        "description": ""
+                    }
+                }
+            },
+            "Options": {
+                "context": {
+                    "Items": [
+                       
+                        {
+                            "Id": 1,
+                            "Name": "No active disease: I currently have no symptoms"
+                        },
+                       
+                        {
+                            "Id": 2,
+                            "Name": "Mild disease: Areas of dry, rough and red skin. The skin itches occasionally. There may be some cracking, flaking of the skin, weeping or oozing of fluid, or bleeding. It has little  impact on everyday activities, such as sleeping, getting dressed, showering and  exercising."
+                        },
+                        {
+                            "Id": 3,
+                            "Name": "extensive areas of dry, rough and red skin. There is also cracking, flaking of the skin, weeping or oozing of fluid, or bleeding. The skin itches at least 3-4 days a week. It has moderate impact on everyday activities, such as sleeping, getting dressed, showering and exercising."
+                        },
+                        {
+                            "Id": 4,
+                            "Name": "Moderate disease: More extensive areas of dry, rough   and red skin. There is also cracking, flaking of the skin, weeping or oozing of fluid, or bleeding. The skin itches atextensive areas of dry, rough and red skin. There is also cracking, flaking of the skin, weeping or oozing of fluid, or bleeding. The skin itches at least 3-4 days a week. It has moderate impact on everyday activities, such as sleeping, getting dressed, showering and exercising."
+                        },
+                          {   "Id": 5,
+                            "Name": "Severe disease: Widespread areas of dry, rough and red skin. There is a lot of cracking, flaking of the skin, weeping or oozing of fluid, or bleeding. The skin itches all the time. It has severe"
+                        }
+                    ]
+                },
+                "selectAll": true
+            },
+            "QuestionnaireId": 1,
+
+        },           
+        {"Id": 4,
+        "Order":4,
+        "Name": "Have you applied corticosteroids (also called 'steroids') for your Atopic Dermatitis (Atopic Eczema) within the last 6 months?",
+        "Code": "QUESTION",
+        "Description": "There are many different types of these medications available that differ in strength. Some require a prescription from a doctor, but others do not. They can be either ointments, creams, lotions, or sprays. Common types of topical corticosteroids (steroids) are for example: Alclometasone (brand name example: Aclovate), Betamethasone (brand name examples: Diprolene, Beta-Val), Clobetasol (brand name example: Temovate)"
+        ,
+        "Schema": {
+            "type": "object",
+            "properties": {
+                "ListProp": {
+                    "type": "number",
+                    "title": "Choose the right Ansver",
+                    "x-display": "radio",
+                    "x-fromData": "context.Items",
+                    "x-itemKey": "Id",
+                    "x-itemTitle": "Name",
+                 }
+            }
+        },
+        "Options": {
+            "context": {
+                "Items": [
+                   
+                    {
+                        "Id": 1,
+                        "Name": "Yes, currently using these"
+                    },
+                   
+                    {
+                        "Id": 2,
+                        "Name": "Yes, but stopped in the last 6 months"
+                    },
+                    {
+                        "Id": 3,
+                        "Name": "Yes, but stopped more than 6 months ago"
+                    },
+                    {   "Id": 4,
+                        "Name": "No, never"
+                    },
+                    {   "Id": 5,
+                        "Name": "Unsure"
+                    }
+                ]
+            },
+            "selectAll": true
+        },
+        "QuestionnaireId": 1,
+
+    }             ,
+            {   "Id": 5,
+                "Order":5,
+                "Name": "Do or did these topical corticosteroids (also called 'steroids') help with your Atopic Dermatitis (Atopic Eczema) symptoms (like dry skin, itching, redness, or skin thickening/cracking?",
+                "Code": "QUESTION",
+                "Description": "",
+                "Schema": {
+                    "type": "object",
+                    "properties": {
+                        "ListProp": {
+                            "type": "number",
+                            "title": "Choose the right Ansver",
+                            "x-display": "radio",
+                            "x-fromData": "context.Items",
+                            "x-itemKey": "Id",
+                            "x-itemTitle": "Name",
+                            "description": ""
+                        }
+                    }
+                },
+                "Options": {
+                    "context": {
+                        "Items": [
+                            {
+                                "Id": 1,
+                                "Name": "They are/were helping very well."
+                            },
+                            {
+                                "Id": 2,
+                                "Name": "They are/were helping, but there are still symptoms"
+                            },                            
+                            {
+                                "Id": 3,
+                                "Name": "They are/were helping, but there are side effects"
+                            },
+                            {
+                                "Id": 4,
+                                "Name": "They are/were helping, but there are still symptoms and side effects"
+                            },
+                            {
+                                "Id": 5,
+                                "Name": "They are hardly helping or not helping at all"
+                            }
+                        ]
+                    },
+                    "selectAll": true
+                },
+                "QuestionnaireId": 1,
+
+            },
+            {   "Id": 6,
+                "Order":6,
+                "Name": "Are you currently pregnant, breastfeeding or planning to become pregnant in the next 6 months?",
+                "Code": "QUESTION",
+                "Description": "",
+                "Schema": {
+                    "type": "object",
+                    "properties": {
+                        "ListProp": {
+                            "type": "number",
+                            "title": "Choose the right Ansver",
+                            "x-display": "radio",
+                            "x-fromData": "context.Items",
+                            "x-itemKey": "Id",
+                            "x-itemTitle": "Name",
+                            "description": ""
+                        }
+                    }
+                },
+                "Options": {
+                    "context": {
+                        "Items": [
+                            {
+                                "Id": 1,
+                                "Name": "Yes."
+                            },
+                            {
+                                "Id": 2,
+                                "Name": "No"
+                            },
+                            {
+                                "Id": 2,
+                                "Name": "Unsure"
+                            }
+                        ]
+                    },
+                    "selectAll": true
+                },
+                "QuestionnaireId": 1,
+
+            }
+        ]
+ 
+    }
+}
---
+++ MVC/Views/QuestionnaireEdit/src/App.vue
@@ -0,0 +1,250 @@
+<template>
+  <v-app id="app" style="width: 100%; padding: 0%" @keydown.ctrl.83.prevent.stop="Save"
+    @keydown.ctrl.37.prevent.stop="SelectPrevQuestion" @keydown.ctrl.39.prevent.stop="SelectNextQuestion">
+    <div class="modal" v-if="loadingData">
+      <img class="loader-icon" :src="require('../../../wwwroot/Content/Images/loading.gif')" />
+    </div>
+    <v-main>
+
+
+      <v-row style="border: outset; padding: 5px; background-color: lightgray">
+        <v-form ref="form" v-model="currentModel.valid" style="width: 100%; padding: 5px">
+          <div style="text-align: center" v-if="currentQuestion != null">
+            {{ currentQuestion.Name }} ({{ currentQuestion.Id }})
+          </div>
+           <div style="text-align: center" v-if="this.Questionnarie.Questions != null">
+            {{ this.Questionnarie.Questions.length }} 
+          </div>         
+          <v-jsf v-if="currentQuestion != null" v-model="currentModel.answerModel" :schema="currentQuestion.Schema"
+            :options="currentQuestion.Options" />
+        </v-form>
+        <v-row style="width: 100%; padding: 5px">
+          <v-spacer></v-spacer>
+          <v-btn style="width: 60px; margin: 3px" @click="SelectPrevQuestion()" :disabled="currentQuestion.Order == 1">
+            <img :src="require('../../../wwwroot/Content/Icons/prev.png')" width="20" height="20" />Prev</v-btn>
+          <v-btn style="width: 60px; margin: 3px" @click="SelectNextQuestion()" :disabled="currentQuestion.Order >= Questionnarie.Questions.length
+            ">
+            Next<img :src="require('../../../wwwroot/Content/Icons/next.png')" width="20" height="20" />
+          </v-btn>
+        </v-row>
+      </v-row>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+if (Id == undefined) {
+  var Id = 1
+}
+import { data } from "./data.js";
+import { baseMixin } from "./BaseMixin.js";
+import VJsf from '@koumoul/vjsf/lib/VJsf.js'
+import '@koumoul/vjsf/lib/VJsf.css'
+import '@koumoul/vjsf/lib/deps/third-party.js'
+const radioOptions = {
+  context: {
+    Items: [
+      { Id: 1, Name: "item 1" },
+      { Id: 2, Name: "item 2" },
+      { Id: 3, Name: "item 3" },
+      { Id: 4, Name: "item 4" },
+    ],
+  },
+  selectAll: true,
+};
+const radioShema = {
+  type: "object",
+  requred: ["ListProp"],
+  properties: {
+    ListProp: {
+      type: "number",
+      title: "Select",
+      "x-display": "radio",
+      "x-fromData": "context.Items",
+      "x-itemKey": "Id",
+      "x-itemTitle": "Name",
+      description: "",
+    },
+  },
+};
+
+export default {
+  name: 'App',
+  mixins: [baseMixin],
+  components: {
+    VJsf
+  },
+  data: () => ({
+    valid: false,
+    currentModel: { answerModel: {}, valid: false },
+    models: [],
+    Questionnarie: { Questions: [] },
+
+    currentQuestion: {
+      Id: 0,
+      ParentId: 1,
+      Name: null,
+      Options: radioOptions,
+      Schema: radioShema,
+    },
+
+  }),
+  methods: {
+    validateForm() {
+      this.$refs.form.validate();
+    },
+
+
+    IsQueryValid(question) {
+
+      if (this.patient && this.patient.id > 0) {
+        let model = this.models.find((x) => x.QuestionId == question.Id);
+
+        return model && model.valid;
+
+      }
+
+      return false;
+    },
+
+
+    SetSelectedStyle(question) {
+      if (this.currentQuestion.Id == question.Id) {
+        return { background: "#ccc" };
+      }
+    },
+    SelectQuestion(question) {
+      if (this.currentQuestion.Id == question.Id) return; //если это повторный вызов
+      if (question.Id == 0) return; //если это фейк  currentQuestion нужный для инициализации Vue
+
+      let prmodel = this.models.find(
+        (x) => x.QuestionId == this.currentQuestion.Id
+      );
+      if (prmodel) {
+        // prmodel.answerModel=this.currentModel;
+        prmodel = this.currentModel;
+      }
+
+      this.currentQuestion = question;
+      this.notValidOptions = null;
+      this.notValidSchema = null;
+      this.SetModel();
+    },
+    SelectNextQuestion() {
+      const nextQuestion = this.Questionnarie.Questions.find(
+        (x) => x.Order == this.currentQuestion.Order + 1
+      );
+      if (nextQuestion) {
+        this.SelectQuestion(nextQuestion);
+      }
+      console.log(nextQuestion) 
+    },
+    SelectPrevQuestion() {
+
+      const prevQuestion = this.Questionnarie.Questions.find(
+        (x) => x.Order == this.currentQuestion.Order - 1
+      );
+      if (prevQuestion) {
+        this.SelectQuestion(prevQuestion);
+      }
+      console.log(prevQuestion)
+    },
+    SetModel() {
+      let model = this.models.find(
+        (x) => x.QuestionId == this.currentQuestion.Id
+      );
+      if (!model) {
+        model = {
+          QuestionId: this.currentQuestion.Id,
+          answerModel: {},
+          valud: false,
+        };
+        this.models.push(model);
+      }
+      this.currentModel = model;
+    },
+
+
+
+    ok(item) {
+      if (item.Errors && item.Errors.length > 0) {
+        this.ShowErrors(item);
+      }
+    },
+    GetQuestions() {
+       console.log('SetQuestions(val)')
+        this.fetch(
+          this.SetQuestions,
+       "Questionnaire/GetQuestions?id=" + Id
+        );
+    //  const callback=this.SetQuestions
+    //   let httpReq = new XMLHttpRequest();
+
+    //   httpReq.open('GET', 'https://localhost:7297/' + "Questionnaire/GetQuestions?id=" + Id);
+    //   httpReq.setRequestHeader('Access-Control-Allow-Origin', 'https://localhost:7297');
+    //   httpReq.setRequestHeader("Content-Type", "application/json"); 
+    //   httpReq.onreadystatechange = function () { 
+    //     if (this.readyState === 4 && this.status == 200 ) 
+    //     {  
+    //       console.log('ok')
+    //       callback(httpReq); 
+    //     }
+    //   }
+    //   httpReq.send()
+      
+    },
+    SetQuestions(val) {
+      console.log(val)
+      if (val.Errors) {
+        this.ShowErrors(val);
+      }
+      if (val.Item) {
+        this.Questionnarie = val.Item; // new QuestionnarieBo (val.Item);
+        if (this.Questionnarie.Questions.length > 0) {
+          this.SelectQuestion(this.Questionnarie.Questions[0]);
+        }
+      }
+    },
+    GetAnswers() {
+      this.fetch(
+        this.SetAnswers,
+        "/Questions/Questionnaire/GetAnsvers?questionnaireId=" +
+        Id +
+        "&patientId=" +
+        this.patient.id
+      );
+    },
+    SetAnswers(val) {
+      console.log(val);
+      if (val.Errors) {
+        this.ShowErrors(val);
+      }
+      if (val.Item) {
+        // this.$nextTick(() => {
+        this.models = val.Item;
+        this.SetModel();
+        // });
+      } else {
+        this.models = [];
+      }
+    },
+    SaveAnsver() {
+      this.fetch(
+        this.ok,
+        "/Questions/Questionnaire/SaveAnsvers?questionnaireId=" +
+        Id +
+        "&patientId=" +
+        this.patient.id,
+        this.models
+      );
+    },
+
+  },
+  mounted: function () {
+   // if (Id) {
+   //   this.GetQuestions();
+   this.SetQuestions(data);
+   // }
+  },
+};
+</script>
